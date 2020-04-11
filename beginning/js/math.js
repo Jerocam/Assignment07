a0cc0eeb4dae58c6c008cb595b3e160a4a442339
Jerocam86 <ocamposjero@gmail.com>
2020-04-10 23:22:18 -0700

    done!

--- beginning/js/math.js
+++ beginning/js/math.js
@@ -2,12 +2,35 @@
 "use strict";
 
 //STEP 1
-
+// let numb = Math.abs(prompt("Enter a number"));
+// window.console.log(numb);
 
 //STEP 2
+// let numb = Math.round(prompt("Enter a number"));
+// window.console.log(numb);
 
 //STEP 3
+// let numb = Math.round(prompt("Enter a number"));
+// window.console.log(numb);
 
 //STEP 4
+// let numb = prompt("Enter 5 numbers - Enter comma on each number");
+// let x = numb.split(",");
+// let arr = [];
+
+// arr.push(x[0]);
+// for(let i =0; i<x.length; i++){
+//     if(x[i] !== x[i-1]){
+//         arr.push(x[i]);
+//     }
+// }
+
+// let lg = Math.max(...arr);
+// let sm = Math.min(...arr);
+
+// window.console.log("The largest of the numbers: "+lg+"\nThe Smallest of the numbers: "+sm);
 
 //STEP 5
+let numb = Math.sqrt(prompt("Enter a number"));
+window.console.log(numb);
+
